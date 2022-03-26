ef2b177535536fb70002a03485de284467006668
JaneHope607 <janehopeodonnell@hotmail.co.uk>
2022-03-26 12:03:33 +0000

    update header and navigation

--- src/components/header.js
+++ src/components/header.js
@@ -2,41 +2,21 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const Header = () => (
+  <header>
+    <div className="container">
+      <div className="logo">
+        <Link to="/">Gavin Reid - Scottish Artist</Link>
+      </div>
+      <div className="navigation">
+        <nav>
+          <Link to="/about">Work</Link>
+          <Link to="/about">Shop</Link>
+          <Link to="/about">Contact</Link>
+        </nav>
+      </div>
     </div>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header

2ddd8e3ec5c64ee62c590dab5b957439b3c1ead4
JaneHope607 <janehopeodonnell@hotmail.co.uk>
2022-03-27 14:41:15 +0100

    Add about blurb below banner

---
+++ src/components/aboutBlurb.js
@@ -0,0 +1,44 @@
+import * as React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+const AboutBlurb = () => {
+    return (
+        <div className="about-blurb">
+            <div className="container">
+                <div className="inner-blurb">
+                    <div className="content">
+                        <h3>Lorum Ipsum Blurb Blurb Blurb</h3>
+                        <p>Blurb Blurb Blurb Blurb Blurb Blurb Blurb
+                        Blurb Blurb Blurb Blurb Blurb Blurb Blurb 
+                        Blurb Blurb Blurb Blurb Blurb Blurb Blurb
+                        Blurb Blurb Blurb Blurb Blurb Blurb Blurb
+                        Blurb Blurb Blurb Blurb Blurb Blurb Blurb
+                        Blurb Blurb Blurb Blurb Blurb Blurb Blurb
+                        Blurb Blurb Blurb Blurb Blurb Blurb Blurb
+                        Blurb Blurb Blurb Blurb Blurb Blurb Blurb
+                        Blurb Blurb Blurb Blurb</p>
+                        <div className="btn-row">
+                            <Link to="/work">View Work</Link>
+                        </div>
+                    </div>
+                    <div className="images">
+                        <div className="top-right">
+                            <StaticImage src="../images/glasgow.jpg"
+                            alt="Barrowlands Glasgow" placeholder="blurred" />
+                        </div>
+                        <div className="top-left">
+                            <StaticImage src="../images/glasgow.jpg"
+                            alt="Barrowlands Glasgow" placeholder="blurred" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="black-box"></div>
+            <div className="black-box overlay"></div>
+        </div>
+    )
+}
+
+export default AboutBlurb
--- src/components/banner.js
+++ src/components/banner.js
@@ -7,13 +7,22 @@ const Banner = () => {
         <div className="banner">
             <div className="container">
                 <div className="row">
+                    <div className="side-image left">
+                        <StaticImage src="../images/glasgow.jpg"
+                        alt="Barrowlands Glasgow" />
+                    </div>
                     <div className="main-text">Gavin Reid</div>
                     <div className="main-image">
-                    <StaticImage src=""
-                    alt="Colourful umbrellas in the sky" />
+                        <StaticImage src="../images/glasgow.jpg"
+                        alt="Barrowlands Glasgow" placeholder="blurred" />
+                    </div>
+                    <div className="side-image right">
+                        <StaticImage src="../images/glasgow.jpg"
+                        alt="Barrowlands Glasgow" />
                     </div>
                 </div>
             </div>
+            <div className="fixed-misc">Scottish Environmental Aritst</div>
         </div>
     )
 }

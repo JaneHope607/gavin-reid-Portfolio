0faa5df1c143e4ffc9ab42e35a5a19e40584a371
JaneHope607 <janehopeodonnell@hotmail.co.uk>
2022-03-27 11:11:03 +0100

    updated banner

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,7 +3,19 @@ import { useStaticQuery, graphql } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image"
 
 const Banner = () => {
-    return <p>Hello</p>
+    return (
+        <div className="banner">
+            <div className="container">
+                <div className="row">
+                    <div className="main-text">Gavin Reid</div>
+                    <div className="main-image">
+                    <StaticImage src=""
+                    alt="Colourful umbrellas in the sky" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Banner
